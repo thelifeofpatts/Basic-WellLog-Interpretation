38390f80c92b397155d0ba98ec552bf12f8de695
arch_linux <koko41701@gmail.com>
2025-02-01 12:21:45 +0700

    add BWLI simple v3

--- src/data_vis/comboPlot.py
+++ src/data_vis/comboPlot.py
@@ -157,3 +157,51 @@ def combo_plot(data, top_depth, bottom_depth, figure_height,subplotadjust,SP_min
         ax23.plot(logs.DT, logs.DEPT, label='DT[us/ft]', color='blue')
         ax23.set_xlabel('DT[us/ft]', color='blue')
         ax23.tick_params(axis='x', colors='blue')
+
+def custom_plot(logs, fig_height, depth_start, depth_end, *custom_logs, units):
+    num_tracks = len(custom_logs)
+    fig, ax = plt.subplots(nrows=1, ncols=num_tracks, figsize=(15, fig_height), sharey=True)
+    fig.suptitle("Custom Interpretation Plot", fontsize=22)
+
+    # Calculate top value with less free space based on the number of tracks
+    top_value = 1 - 0.015 * num_tracks
+
+    # Adjust top parameter to leave less free space at the top
+    fig.subplots_adjust(top=top_value, wspace=0.2)
+
+    units = units  # in list
+
+    depth_range = depth_end - depth_start
+
+    # Dynamically adjust tick intervals based on the depth range
+    if depth_range > 5000:
+        major_tick_interval = 1000
+        minor_tick_interval = 100
+    elif depth_range > 1000:
+        major_tick_interval = 500
+        minor_tick_interval = 50
+    else:
+        major_tick_interval = 100
+        minor_tick_interval = 10
+
+    # General setting for all axes
+    for axes in ax:
+        axes.set_ylim(depth_start, depth_end)
+        axes.invert_yaxis()
+        axes.yaxis.grid(True, which='minor', linestyle=':')
+        axes.yaxis.grid(True, which='major', linestyle='-', linewidth='1')
+        axes.yaxis.set_major_locator(ticker.MultipleLocator(major_tick_interval))
+        axes.yaxis.set_minor_locator(ticker.MultipleLocator(minor_tick_interval))
+        axes.get_xaxis().set_visible(False)
+
+    # Customizing each track with unit and color
+    for i, log_name in enumerate(custom_logs):
+        current_ax = ax[i].twiny()
+        current_ax.plot(logs[log_name], logs.DEPT, label=f'{log_name} [{units[i]}]', color='C'+str(i))
+        if log_name == "DR" or log_name == "MR" or log_name == "SR":
+            current_ax.set_xscale('log')
+        current_ax.set_xlabel(f'{log_name} [{units[i]}]', color='C'+str(i))
+        current_ax.tick_params(axis='x', colors='C'+str(i))
+        current_ax.spines['top'].set_position(('outward',0))
+        current_ax.legend(loc='lower right', facecolor='white', framealpha=1, fontsize=7)
+    plt.show()
--- src/data_vis/interpretPlot.py
+++ src/data_vis/interpretPlot.py
@@ -3,8 +3,8 @@ import matplotlib.ticker as ticker
 import numpy as np
 import scipy
 
-def interpretation_plot(logs, core_selected_data, depth_start, depth_end, SP_min, SP_max, CALI_min, CALI_max, GR_min, GR_max, DR_min, DR_max, MR_min, MR_max, SR_min, SR_max, RHOB_min, RHOB_max, NPHI_min, NPHI_max, DT_min, DT_max, perm,fill):
-    fig, ax = plt.subplots(nrows=1, ncols=7, figsize=(12,20), sharey=True)
+def interpretation_plot(logs, core_selected_data, fig_height, depth_start, depth_end, SP_min, SP_max, CALI_min, CALI_max, GR_min, GR_max, DR_min, DR_max, MR_min, MR_max, SR_min, SR_max, RHOB_min, RHOB_max, NPHI_min, NPHI_max, DT_min, DT_max, perm,fill):
+    fig, ax = plt.subplots(nrows=1, ncols=7, figsize=(12,fig_height), sharey=True)
     fig.suptitle("Interpretation Plot", fontsize=22)
     fig.subplots_adjust(top=0.85,wspace=0.2)
 
@@ -221,55 +221,6 @@ def interpretation_plot(logs, core_selected_data, depth_start, depth_end, SP_min
     ax62.set_xlabel('Matrix', color='orange')
     ax62.tick_params(axis='x', colors='orange')
     ax62.legend(loc='lower left')
-
-
-def custom_interpretation_plot(logs, depth_start, depth_end, *custom_logs, units):
-    num_tracks = len(custom_logs)
-    fig, ax = plt.subplots(nrows=1, ncols=num_tracks, figsize=(15, 20), sharey=True)
-    fig.suptitle("Custom Interpretation Plot", fontsize=22)
-
-    # Calculate top value with less free space based on the number of tracks
-    top_value = 1 - 0.015 * num_tracks
-
-    # Adjust top parameter to leave less free space at the top
-    fig.subplots_adjust(top=top_value, wspace=0.2)
-
-    units = units  # in list
-
-    depth_range = depth_end - depth_start
-
-    # Dynamically adjust tick intervals based on the depth range
-    if depth_range > 5000:
-        major_tick_interval = 1000
-        minor_tick_interval = 100
-    elif depth_range > 1000:
-        major_tick_interval = 500
-        minor_tick_interval = 50
-    else:
-        major_tick_interval = 100
-        minor_tick_interval = 10
-
-    # General setting for all axes
-    for axes in ax:
-        axes.set_ylim(depth_start, depth_end)
-        axes.invert_yaxis()
-        axes.yaxis.grid(True, which='minor', linestyle=':')
-        axes.yaxis.grid(True, which='major', linestyle='-', linewidth='1')
-        axes.yaxis.set_major_locator(ticker.MultipleLocator(major_tick_interval))
-        axes.yaxis.set_minor_locator(ticker.MultipleLocator(minor_tick_interval))
-        axes.get_xaxis().set_visible(False)
-
-    # Customizing each track with unit and color
-    for i, log_name in enumerate(custom_logs):
-        current_ax = ax[i].twiny()
-        current_ax.plot(logs[log_name], logs.DEPT, label=f'{log_name} [{units[i]}]', color='C'+str(i))
-        if log_name == "DR" or log_name == "MR" or log_name == "SR":
-            current_ax.set_xscale('log')
-        current_ax.set_xlabel(f'{log_name} [{units[i]}]', color='C'+str(i))
-        current_ax.tick_params(axis='x', colors='C'+str(i))
-        current_ax.spines['top'].set_position(('outward',0))
-        current_ax.legend(loc='lower right', facecolor='white', framealpha=1, fontsize=7)
-    plt.show()
 
 def pickett_plot(logs, vcl_limit, a, rwa, m, n, z):
     plt.figure(figsize=(7,6))
